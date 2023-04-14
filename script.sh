1c00cf117aba0a77754420a6eddd0ab5eb256b72
Dramane-H <hidramane@yahoo.fr>
2023-04-14 22:26:36 +0200

    Automated CI/CD pipeline for Java Project

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee -a /etc/apt/sources.list.d/jenkins.list
-sudo apt update
-sudo apt install jenkins -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 
